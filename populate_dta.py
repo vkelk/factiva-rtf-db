76552d32713dbde29803358ec291802f1d3308ab
Vasko Kelkocev <pisimina@gmail.com>
2019-01-30 22:19:52 +0100

    pupulate data from analysis

--- populate_dta.py
+++ populate_dta.py
@@ -1,7 +1,7 @@
 import re
 import pandas as pd
 
-from db_helper import Session, Articles, CompanyArticle
+from db_helper import Session, Articles, CompanyArticle, Analysis
 
 
 file_path = 'sample_file.dta'
@@ -16,23 +16,48 @@ def get_articles(gvket, date):
     return q.all()
 
 
+def get_analysis(article_id):
+    session = Session()
+    q = session.query(Analysis).filter(Analysis.id == article_id)
+    if q is not None:
+        return q.first()
+    return None
+
+
 def slugify(s):
     return re.sub('[^\w]+', '_', s).strip().lower()[:32]
 
 
 if __name__ == '__main__':
     df = pd.read_stata(file_path)
+    # df.to_csv('output1.csv')
+    # exit()
+    # df_output = pd.DataFrame()
+    # print(df.head())
     for i in df.index:
+        # print(df.iloc[i])
+        # exit()
         gvkey = str(df.at[i, 'gvkey']).rstrip('.0')
         date = df.at[i, 'date'].date()
         print(gvkey, date)
         articles = get_articles(gvkey, date)
         if len(articles) > 0:
-            df.at[i, 'daily_articles'] = len(articles)
+            df.at[i, 'news_count'] = len(articles)
             categories = []
+            word_count = 0
+            positive = 0
+            negative = 0
+            uncertain = 0
             for article in articles:
                 categories.append(article.main_category)
                 categories.append(article.sub_category)
+                analysis_data = get_analysis(article.article_id)
+                if analysis_data is not None:
+                    word_count += analysis_data.word_count
+                    positive += analysis_data.positive
+                    negative += analysis_data.negative
+                    uncertain += analysis_data.uncertainty
+            df.at[i, 'word_count'] = word_count
             categories = set(categories)
             for category in categories:
                 if category is not None and len(category.strip()) > 0:
